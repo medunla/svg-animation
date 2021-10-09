574c6e125c773fdf07cd7003ab21b67e6c1e86f0
PanupatK <panupatnew@gmail.com>
2021-10-09 17:54:04 +0700

    Setup config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/standard'
+	],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'indent': ['error', 'tab'],
+		'no-tabs': ['error', { 'allowIndentationTabs': true }],
+		'comma-dangle': ['error', 'always-multiline'],
+		'semi': ['error', 'always'],
+		'no-else-return': 'error',
+		'default-case': 'error',
+		'no-lonely-if': 'error',
+	}
 }
